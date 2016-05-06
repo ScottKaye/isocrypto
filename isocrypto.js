e6156ec7f9764fb9dd2b2bd6f6cb4968f7a6fee9
Scott Kaye <pgreviews@gmail.com>
2016-05-06 00:01:09 -0400

    No

--- isocrypto.js
+++ isocrypto.js
@@ -4,14 +4,3 @@ const Isocrypto = {
 };
 
 module.exports = Isocrypto;
-
-Isocrypto.Asymmetric.generateKeys().then(keys => {
-	let data = "Test data";
-	console.log("Asymmetric encrypting:", data)
-	Isocrypto.Asymmetric.encrypt(keys.public, data).then(encrypted => {
-		console.log("Asymmetric Encrypted:", encrypted.length, "bytes");
-		Isocrypto.Asymmetric.decrypt(keys.private, encrypted).then(decrypted => {
-			console.log("Asymmetric Decrypted:", decrypted);
-		}).catch(e => console.log(e));
-	});
-});
